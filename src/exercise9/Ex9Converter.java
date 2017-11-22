a88e215cee2940e1a0949c5ce4f1a792569afc30
[tappma01] <[tappma01@luther.edu]>
2017-11-21 21:33:56 -0600

    Working on Exercise9

--- src/exercise9/Ex9Converter.java
+++ src/exercise9/Ex9Converter.java
@@ -6,7 +6,7 @@ package exercise9;
  */
 public class Ex9Converter {
   public Double c2f(Double valueFrom) {
-      return (valueFrom * (9/5) + 32);
+      return valueFrom * 9/5 + 32;
   }
 
   public Double c2k(Double valueFrom) {
@@ -14,11 +14,11 @@ public class Ex9Converter {
   }
 
   public Double f2c(Double valueFrom) {
-      return ((valueFrom - 32) / (9/5));
+      return (valueFrom - 32) / 1.8;
   }
 
   public Double f2k(Double valueFrom) {
-      return ((valueFrom - 32) / (9/5)) + 273.15;
+      return (valueFrom - 32) / 1.8 + 273.15;
   }
 
   public Double k2c(Double valueFrom) {
@@ -26,6 +26,6 @@ public class Ex9Converter {
   }
 
   public Double k2f(Double valueFrom) {
-      return ((valueFrom - 273.15) * (9/5) + 32);
+      return (valueFrom - 273.15) * 1.8 + 32;
   }
 }
