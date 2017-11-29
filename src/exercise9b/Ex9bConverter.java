debd5f955e2b086e2bb50a43f9191f791177a75a
[tappma01] <[tappma01@luther.edu]>
2017-11-28 22:41:56 -0600

    Working on Exercise9b

---
+++ src/exercise9b/Ex9bConverter.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise9b;
+
+/**
+ *
+ * @author tappma01
+ */
+public class Ex9bConverter {
+    public Double mi2km(Double valueFrom) {
+      return valueFrom * 1.609;
+  }
+    public Double km2mi(Double valueFrom) {
+      return valueFrom / 1.609;
+  }
+    public Double insq2cmsq(Double valueFrom) {
+      return valueFrom * 6.452;
+  }
+    public Double cmsq2insq(Double valueFrom) {
+      return valueFrom / 6.452;
+  }
+    public Double floz2ml(Double valueFrom) {
+      return valueFrom * 29.5735;
+  }
+    public Double ml2floz(Double valueFrom) {
+      return valueFrom / 29.5735;
+  }
+    public Double lb2kg(Double valueFrom) {
+      return valueFrom * 0.453592;
+  }
+    public Double kg2lb(Double valueFrom) {
+      return valueFrom / 0.453592;
+  }
+    public Double pa2torr(Double valueFrom) {
+      return valueFrom * 0.00750061683;
+  }
+    public Double torr2pa(Double valueFrom) {
+      return valueFrom * 133.322368;
+  }
+    
+}
