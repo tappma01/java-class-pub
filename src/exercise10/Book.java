c916207688bf8fd2464c3286eacde481599b73ed
[tappma01] <[tappma01@luther.edu]>
2017-11-30 21:13:38 -0600

    Working on Exercise9b

--- src/exercise10/Book.java
+++ src/exercise10/Book.java
@@ -1,20 +1,135 @@
 package exercise10;
 
+import java.util.Comparator;
+import java.util.Objects;
+
 /**
  * Class Book
  * @author yasiro01
  */
 public class Book {
+    protected final String title;
+    protected final String author;
+    protected Double price;
+    protected final Integer year;
+
+
+public Book(String title, String author, Double price, Integer year){
+    this.title = title;
+    this.author = author;
+    this.price = price;
+    this.year = year;
 }
 
-class ByTitle implements Comparator<Book> {
+public Book(Book otherBook){
+    this.title = otherBook.title;
+    this.author = otherBook.author;
+    this.price = otherBook.price;
+    this.year = otherBook.year;
 }
 
-class ByAuthor implements Comparator<Book> {
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 11 * hash + Objects.hashCode(this.title);
+        hash = 11 * hash + Objects.hashCode(this.author);
+        hash = 11 * hash + (int) (Double.doubleToLongBits(this.price) ^ (Double.doubleToLongBits(this.price) >>> 32));
+        hash = 11 * hash + Objects.hashCode(this.year);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Book other = (Book) obj;
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        if (!Objects.equals(this.year, other.year)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return title + " (" + year + ") by " + author + " costs $" + price;
+    }
+
+
+
+
+
 }
 
 class ByPrice implements Comparator<Book> {
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        if (o1.price < o2.price) return -1;
+        if (o1.price > o2.price) return 1;
+        return 0;
+    }
 }
 
-class ByYear implements Comparator<Book> {
+
+class ByTitle implements Comparator<Book> {
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.title.compareTo(o2.title);
+    }
 }
+
+class ByAuthor implements Comparator<Book> {
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.author.compareTo(o2.author);
+    }
+}
+
+
+class ByYear implements Comparator<Book> {
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.year.compareTo(o2.year);
+    }
+}
+
+
+
+
--- src/exercise10/BookShop.java
+++ src/exercise10/BookShop.java
@@ -17,32 +17,68 @@ import java.util.regex.Pattern;
  * @author yasiro01
  */
 public class BookShop {
+    protected ArrayList<Book> catalog;
 
   public BookShop() {
+       this.catalog = new ArrayList<Book>();
   }
   
   public BookShop(String filename) throws FileNotFoundException {
+      this();
+      BufferedReader reader = new BufferedReader(new FileReader(filename));
+      try{
+          String[] line;
+          Book newBook;
+          while (reader.ready()){
+          line = reader.readLine().split(",");
+          newBook = new Book(line[0],line[1], Double.parseDouble(line[2]), Integer.parseInt(line[3]));
+          catalog.add(newBook);
+          }
+      reader.close();
+      
+  }
+      catch (IOException ex) {
+          Logger.getLogger(BookShop.class.getName()).log(Level.SEVERE, null, ex);
+          System.out.println("Error");
+      }
   }
   
   public BookShop(BookShop anotherBookShop) {
+      this();
+      for(Book book: anotherBookShop.catalog){
+          catalog.add(new Book(book));
+      }
   }
   
   public void addNewTitle(Book book) {
+      catalog.add(book);
   }
   
   public int size() {
+      return catalog.size();
   }
   
   public void discountAll(Double discountPercent) {
+       for(Book bk: catalog){
+           double originPrice = bk.getPrice();
+           double newPercent = (100 - discountPercent) / 100;
+           double newPrice = originPrice * newPercent;
+           bk.setPrice(newPrice);
+      }
   }
   
   public void printCatalog() {
+      for(Book bk: catalog){
+          System.out.println(bk);
+      }
   }
   
   public void order(Comparator<Book> comp) {
+      catalog.sort(comp);
   }
   
   public ArrayList<Book> getCatalog() {
+      return catalog;
   }
 
 }
