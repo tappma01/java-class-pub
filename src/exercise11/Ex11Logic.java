46689ec7d8277ebe5286eb8345dac80977aab285
[tappma01] <[tappma01@luther.edu]>
2017-12-02 15:09:09 -0600

    Working on Exercise11

--- src/exercise11/Ex11Logic.java
+++ src/exercise11/Ex11Logic.java
@@ -1,6 +1,7 @@
 package exercise11;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * Exercise 11 logic
@@ -14,6 +15,10 @@ public class Ex11Logic {
    * @param item
    * @return length of the parameter's string representation
    */
+    public <T> int strLen(T item){
+        String item2 = item.toString();
+        return item2.length();
+    }
   // TODO: Implement method strLen
   
   /**
@@ -24,6 +29,22 @@ public class Ex11Logic {
    * @param item3
    * @return the greater of three parameters
    */
+    public <T extends Comparable> T greater(T item1, T item2, T item3) {
+        if (item1.compareTo(item2) >= 0) {
+            if(item1.compareTo(item3) >= 0){
+                return item1;
+            }
+        }
+        if(item2.compareTo(item1) >= 0) {
+            if(item2.compareTo(item3) >= 0){
+                return item2;
+            }
+        }
+        
+        return item3;
+    }
+    
+    
   // TODO: Implement method greater
   
   /**
@@ -32,6 +53,11 @@ public class Ex11Logic {
    * @param list of objects
    * print a list of items
    */
+  static <S, T extends Iterable<S>> void printList(T list){
+      System.out.print(list.toString());
+      System.out.print(" ");
+      
+  }
   // TODO: Implement method printList
   
   /**
@@ -40,9 +66,31 @@ public class Ex11Logic {
    * @param list of objects
    * @return the smallest item in the collection
    */
+  
+    private <T> T lessThan(T i, T j) {
+    T min = i;
+    if(min.hashCode() > j.hashCode()){
+        min = j;
+    }
+    return min;
+    }
+  
+  public <T> T findMinItem(T list) {
+      Collection<T> list1 = (Collection<T>) list;
+      Iterator<T> num = list1.iterator();
+      T minimum = num.next();
+      Iterator<T> it = list1.iterator();
+      for (int i = 0; i < list1.size(); i++){
+          T value = it.next();
+          minimum = this.lessThan(minimum, value);
+      }
+      return minimum;
+            
   // TODO: Implement method findMinItem
 }
 
+
+}
   /**
    * Task 5
    * @param <T>
@@ -50,4 +98,36 @@ public class Ex11Logic {
    * A class with two data members, T fuzz and U buzz
    * Implement the constructor, getters, setters, and toString
    */
+    class Foo <T, U> {
+        private T fuzz;
+        private U buzz;
+
+    public Foo(T fuzz, U buzz) {
+        this.fuzz = fuzz;
+        this.buzz = buzz;
+    }
+
+    public T getFuzz() {
+        return fuzz;
+    }
+
+    public U getBuzz() {
+        return buzz;
+    }
+
+    public void setFuzz(T fuzz) {
+        this.fuzz = fuzz;
+    }
+
+    public void setBuzz(U buzz) {
+        this.buzz = buzz;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" + "fuzz=" + fuzz + ", buzz=" + buzz + '}';
+    }
+        
+    }
+
   // TODO: Implement class Foo
